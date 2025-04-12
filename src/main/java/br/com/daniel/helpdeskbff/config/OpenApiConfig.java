d81c723fd52e46afada820300738edf24cb14971
Daniel Paulino <daniel-paulino@hotmail.com>
2025-04-12 01:44:53 -0300

    feat: added Swagger security config

--- src/main/java/br/com/daniel/helpdeskbff/config/OpenApiConfig.java
+++ src/main/java/br/com/daniel/helpdeskbff/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package br.com.daniel.helpdeskbff.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +28,10 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title(title)
                         .description(description)
-                        .version(version));
+                        .version(version))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
 }
